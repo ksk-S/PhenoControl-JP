244def5062d2cdc9ac6c328a203630a360ff039c
Ryuta Aoki <ryuta.aoki@volocitee.com>
2024-06-11 20:04:56 +0900

    Modified to execute app at current dir.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ PORT=${FLASK_PORT:-8080}
 PID_FILE="app.${PORT}.pid"
 
 # Activate the virtual environment
-if source /home/ec2-user/PhenoControl-JP/venv/bin/activate; then
+if source venv/bin/activate; then
     echo "Virtual environment activated."
 else
     echo "Error: Failed to activate virtual environment."
@@ -17,7 +17,7 @@ fi
 export PYTHONUNBUFFERED=1
 
 # Run the application and create a PID file
-python /home/ec2-user/PhenoControl-JP/app.py --port=$PORT &
+python app.py --port=$PORT &
 echo $! > $PID_FILE
 
 # Wait for the application to finish
